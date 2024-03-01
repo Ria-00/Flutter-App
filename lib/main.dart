5e1bc7e4029b0bec0db8b57ab4d3c6c2c43c9373
Riya Singh <singhriyaa006@gmail.com>
2024-03-01 12:58:28 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp( 
+    MaterialApp(
+      home: MyApp(),
+      routes: {'/signup':(context) => },
+    ));
 }
 
 class MyApp extends StatelessWidget {
@@ -13,15 +17,15 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.deepOrange),
       home: Scaffold(
-        appBar: AppBar(title: const Text('My App'),),
+        appBar: AppBar(title: Center(child:Text('LOGIN') ,) ,),
         // body: const Center(child: Text('Content',style: TextStyle(fontSize: 34),),),
         body: SingleChildScrollView(
           child: Container(
             padding: EdgeInsets.all(12),
             child: Column(children: [
-            Text('SUSTAINIFY',style: TextStyle(fontSize: 34),),
+            // Text('SUSTAINIFY',style: TextStyle(fontSize: 34),),
             SizedBox(height: 34,),
-            Image.network('https://images.pexels.com/photos/3735218/pexels-photo-3735218.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+            Image.network('https://seeklogo.com/images/F/flutter-logo-5086DD11C5-seeklogo.com.png',height: 200,),
             SizedBox(height: 30,),
             TextField(
               decoration: InputDecoration(labelText:'Username:'),
@@ -30,11 +34,39 @@ class MyApp extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText:'Password:'),obscureText: true,
             ),
-            ElevatedButton(onPressed: ()=>(), child: Text('Login'))
+            SizedBox(height: 40,),
+            ElevatedButton(onPressed: ()=>(), 
+              child: Text('Login'),
+              style: ElevatedButton.styleFrom(
+                elevation: 8.0,
+                minimumSize:const Size(350, 50),
+                shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(10)),
+                backgroundColor:Color.fromRGBO(20, 177, 255, 0.612),
+                foregroundColor: Color.fromRGBO(255, 255, 255, 0.612)
+                ),),
+            SizedBox(height: 20,),
+            Text("Forgot Password? Click here",),
+            SizedBox(height: 20,),
+            TextButton(onPressed: ()=>(), child: Text('No Account? Sign Up'),
+            style: ElevatedButton.styleFrom(
+                minimumSize:const Size(350, 50),
+                shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(5)),
+                backgroundColor:Color.fromRGBO(147, 149, 150, 0.612),
+                foregroundColor: Color.fromRGBO(18, 18, 18, 0.612)
+                ),)
+
           ],),),
         ),
       ),
     );
+  }  
+}
+
+class SignUp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
   }
-    
+
 }
